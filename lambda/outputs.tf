825f42637e844e362585093e632710405b2ee037
Karsten Jakobsen <kj@evercall.dk>
2017-02-25 18:57:14 +0100

    Added API endpoint for test

--- lambda/outputs.tf
+++ lambda/outputs.tf
@@ -6,3 +6,8 @@
 output "auto_deployment_filename" {
   value = "${aws_lambda_function.lambda-dummy.function_name}.zip"
 }
+
+# Function name
+output "function_name" {
+  value = "${aws_lambda_function.lambda-dummy.function_name}"
+}
