38f956ed325a4b519b1226cc5cfe8f2212bb4785
Karsten Jakobsen <kj@evercall.dk>
2017-03-22 14:52:41 +0100

    Added new module for resources

--- api-lambda-proxy-resource/main.tf
+++ api-lambda-proxy-resource/main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_api_gateway_method" "proxy_request_method" {
 
-  rest_api_id   = "${aws_api_gateway_rest_api.proxy_api.id}"
+  rest_api_id   = "${var.api_id}"
   resource_id   = "${aws_api_gateway_resource.proxy_api_resource.id}"
   http_method   = "ANY"
   authorization = "${var.authorization}"
@@ -13,7 +13,7 @@ resource "aws_api_gateway_method" "proxy_request_method" {
 
 resource "aws_api_gateway_integration" "request_method_integration" {
 
-  rest_api_id = "${aws_api_gateway_rest_api.proxy_api.id}"
+  rest_api_id = "${var.api_id}"
   resource_id = "${aws_api_gateway_resource.proxy_api_resource.id}"
   http_method = "${aws_api_gateway_method.proxy_request_method.http_method}"
   type        = "AWS_PROXY"
@@ -30,41 +30,32 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.proxy_api.id}/*/*"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${var.api_id}/*/*"
 
 }
 
-# Now, we need an API to expose those functions publicly
-resource "aws_api_gateway_rest_api" "proxy_api" {
-  name = "${var.api_name}"
-}
-
-  resource "aws_api_gateway_resource" "proxy_api_resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.proxy_api.id}"
-  parent_id   = "${aws_api_gateway_rest_api.proxy_api.root_resource_id}"
+resource "aws_api_gateway_resource" "proxy_api_resource" {
+  rest_api_id = "${var.api_id}"
+  parent_id   = "${aws_api_gateway_resource.api_resource.id}"
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_resource" "api_resource" {
+  rest_api_id = "${var.api_id}"
+  parent_id   = "${var.root_resource_id}"
+  path_part   = "${var.resource_path}"
+}
+
 resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "${aws_api_gateway_rest_api.proxy_api.id}"
+  rest_api_id = "${var.api_id}"
   resource_id = "${aws_api_gateway_resource.proxy_api_resource.id}"
   http_method = "ANY"
   status_code = "200"
 }
 
 resource "aws_api_gateway_integration_response" "proxy_api_integration_response" {
-  rest_api_id = "${aws_api_gateway_rest_api.proxy_api.id}"
+  rest_api_id = "${var.api_id}"
   resource_id = "${aws_api_gateway_resource.proxy_api_resource.id}"
   http_method = "ANY"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
 }
-
-# We can deploy the API now! (i.e. make it publicly available)
-resource "aws_api_gateway_deployment" "proxy_api_deployment" {
-
-  depends_on  = ["aws_api_gateway_method_response.200"]
-  rest_api_id = "${aws_api_gateway_rest_api.proxy_api.id}"
-  stage_name  = "v1"
-
-}
-
