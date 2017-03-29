e6f0ac60a94a8b50880eaabbf5a7274a1a4035a7
Karsten Jakobsen <kj@evercall.dk>
2017-03-29 14:02:19 +0200

    Changed filename

--- lambda-deploy/outputs.tf
+++ lambda-deploy/outputs.tf
@@ -6,3 +6,8 @@
 output "bucket_arn" {
   value = "${aws_s3_bucket.lambda-deploy-bucket.arn}"
 }
+
+# Filename for autodeploy
+output "auto_deploy_bucket_name" {
+  value = "${aws_s3_bucket.lambda-deploy-bucket.bucket}"
+}
